2f908e277437c7d09f53a837d4aed18980d7e713
Maura Slavin <MauraMSlavin@gmail.com>
2019-11-20 20:22:41 -0500

    Got stars count.

--- index.js
+++ index.js
@@ -1,72 +1,99 @@
-// inquirer is used to prompt the user for the Github username and favorite color
-const inquirer = require('inquirer');
 // axios is used for api queries
 const axios = require("axios");
-
-
+// inquirer is used to prompt the user for the Github username and favorite color
+const inquirer = require('inquirer');
 
 // ask the user for the Github username and color
 inquirer
     .prompt([
         {
             type: "input",
-            message: "What is the Github username of the person whose profile you would like?",
-            name: "username"
+            name: "username",
+            message: "What is the Github username of the person whose profile you would like?"
         },
         {
             type: 'list',
-            name: 'color',
+            name: 'favColor',
             message: 'Click on your favorite color.',
             choices: ['blue', 'green', 'red', 'purple', 'yellow', 'pink', 'orange'],
-            filter: function(val) {
+            filter: function (val) {
                 return val.toLowerCase();
             }
         }
     ])
-    .then((promptResponse) => {
-        //  can now get repositories from Github        
-        //      console.log(`Username entered: ${response.username}`);
-        //    console.log(`Color entered: ${response.color}`);
-        const queryUrlRepositories = `https://api.github.com/users/${promptResponse.username}/repos?per_page=100`;
+    .then((promptResponses) => {
+        // get username & color from response
+        const { username, favColor } = promptResponses;
 
-        axios.get(queryUrlRepositories).then(function (responseRepositories) {
-            // console.log(response.data);   // ALL the results!!
+        //  can now query Github...for user's profile information   
+        // query Github for 
+        const queryUrlOwner = `https://api.github.com/users/${username}`;
 
-            // Get all the repository names
-            const repositoryNames = responseRepositories.data.map(function (repository) {
-                // console.log("Repo.name:");  // this happens once for each repository
-                // console.log(repo.name);
-                return repository.name;
+        axios.get(queryUrlOwner).then(function (responseOwner) {
+
+            //get information needed
+            // profile profile link, profile image link, location, bio, blog link, # repositories
+            //   console.log("responseOwner(.data)");
+            // console.log(responseOwner.data);
+
+            const githubProfileUrl = responseOwner.data.html_url;
+            const profileImageUrl = responseOwner.data.avatar_url;
+            const location = responseOwner.data.location;
+            const bio = responseOwner.data.bio;
+            const blogUrl = responseOwner.data.blog;
+            const numRepos = responseOwner.data.public_repos;
+
+
+            console.log("Profile:  " + githubProfileUrl);
+            console.log("Image:  " + profileImageUrl);
+            console.log("location:  " + location);
+            console.log("bio: " + bio);
+            console.log("blog:  " + blogUrl);
+            console.log("# repos:  " + numRepos);
+            console.log("starred: ");
+
+
+
+            // query Github for user's repositories
+            const queryUrlRepositories = `https://api.github.com/users/${username}/repos?per_page=100`;
+
+            axios.get(queryUrlRepositories).then(function (responseRepositories) {
+                // console.log(responseRepositories.data);   // ALL the results!!
+
+                // Get star counts
+                count = 0;
+                const repositoryStars = responseRepositories.data.map(function (repository) {
+                    console.log("responseRepositories.stargazers_count:" + repository.stargazers_count);  // this happens once for each repository
+                    // console.log(repo.name);
+                    count += repository.stargazers_count;
+                    console.log("count: " + count);
+                    console.log(" ");
+                    return repository.stargazers_count;
+                });
+
+              //  console.log("repo names:  " + repositoryNames);
+                //console.log("First repo name:  " + repositoryNames[0]);
+
+
+
+
+                //        console.log(`Saved ${repoNames.length} repos`);
             });
+            //  console.log(`First repo:  ${repoNames[0]}`);
+            //  console.log(`First repo data:`);
+            //  console.log(res.data[0]);
+            //  console.log("First repo owner:");
+            //  console.log(res.data[0].owner);
+            //    console.log(`Profile image URL:  ${profileImageURL}`);
+            //       });
+            //  if (response.confirm === response.password) {
+            //    console.log("Success!");
+            //  }
+            //  else {
+            //    console.log("You forgot your password already?!");
+            //  }
 
-            
 
-
-            console.log("repo names:  " + repositoryNames);
-            console.log("First repo name:  " + repositoryNames[0]);
-
-            let profileImageURL = responseRepositories.data[0].owner.avatar_url;
-
-
-            //        console.log(`Saved ${repoNames.length} repos`);
         });
-        //  console.log(`First repo:  ${repoNames[0]}`);
-        //  console.log(`First repo data:`);
-        //  console.log(res.data[0]);
-        //  console.log("First repo owner:");
-        //  console.log(res.data[0].owner);
-        //    console.log(`Profile image URL:  ${profileImageURL}`);
-        //       });
-        //  if (response.confirm === response.password) {
-        //    console.log("Success!");
-        //  }
-        //  else {
-        //    console.log("You forgot your password already?!");
-        //  }
-
 
     });
-
-
-
-
